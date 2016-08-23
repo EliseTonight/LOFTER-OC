963ee0074ae6ae6f3b55c132ec225f3e26623c29
EliseTonight <951150127@qq.com>
2016-08-23 20:57:03 +0800

    Initial Commit

---
+++ LofterOC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LofterOC
+//
+//  Created by Elise on 16/8/23.
+//  Copyright © 2016年 Elise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
